1ad21523b307ada17ebe6037391da3996a4f7370
sahira firaj <shrafrz265@gmail.com>
2026-07-28 12:23:11 +0700

    belum selesai

---
+++ config/koneksi.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'warung_abc';
+
+$koneksi = mysqli_connect($host, $user, $password, $database);
+
+if (!$koneksi) {
+    die('Koneksi database gagal: ' . mysqli_connect_error());
+}
+?>
