a23890f58189f8628786ef28764eadc6212d3159
sahirafiraj <shrafrz265@gmail.com>
2026-08-04 12:21:53 +0700

    belum selesai

--- dashboard.php
+++ dashboard.php
@@ -10,6 +10,16 @@ include 'includes/cek_session.php';
 <body>
     <h1>Selamat Datang, <?php echo $_SESSION['nama_lengkap']; ?></h1>
     <p>Anda Login sebagai: <?php echo $_SESSION['role']; ?></p>
+
+    <ul>
+        <?php if($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'gudang') { ?>
+    <?php } ?>
+
+    <?php if($_SESSION['role']== 'admin' || $_SESSION['role']== 'kasir') { ?>
+        <li><a href="transaksi.php">Transaksi Kasir</a></li>
+        <li><a href="riwayat_transaksi.php">Riwayat Transaksi</a></li>
+    <?php } ?>
+    </ul>
     <a href="logout.php">Logout</a>
 </body>
 </html>
